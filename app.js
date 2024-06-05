b37dc834dc42ac6b7da7b676826f830afdbd476e
Valeri <i450168@fhict.nl>
2024-06-05 17:14:55 +0200

    Testing pipeline

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://fantastic-zabaione-e18357.netlify.app"); // "Replace with: for local http://localhost:5173"
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, content-type, Accept, Authorization");
   res.header('Access-Control-Allow-Credentials', true);
@@ -35,33 +35,3 @@ app.use('/api/user', require('./controllers/userRoutes'))
 app.use(customErrors)
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const business_regex = new RegExp(
-//   "https://capitalplusbrokers.com/business/[^/]+/$"
-// );
